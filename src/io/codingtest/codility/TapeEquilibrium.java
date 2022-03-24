2b30110ce4574469294c72dec3564b2d6fb1e702
bisori <bisori78@gmail.com>
2022-03-24 09:29:41 +0900

    solved TapeEquilibrium problem

---
+++ src/io/codingtest/codility/TapeEquilibrium.java
@@ -0,0 +1,30 @@
+package io.codingtest.codility;
+
+/**
+ * https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
+ */
+public class TapeEquilibrium {
+
+    public static void main(String[] args){
+        int[] input = new int[]{-1000,1000};
+        int output = new TapeEquilibrium().solution(input);
+        System.out.println(output);
+    }
+
+    public int solution(int[] A) {
+        int leftPart = 0;
+        int rightPart = 0;
+        for (int i = 0 ; i < A.length ; i++) {
+            rightPart += A[i];
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0 ; i < A.length-1 ; i++) {
+            leftPart = leftPart + A[i];
+            rightPart = rightPart - + A[i];
+            min = Math.min(min, Math.abs(leftPart - rightPart));
+        }
+
+        return min;
+    }
+}
