c9d52db8bf444e4edc4764950073f84149ee41f3
bisori <bisori78@gmail.com>
2022-03-25 06:16:14 +0900

    solved FrogRiverOne, PermCheck

---
+++ src/io/codingtest/codility/FrogRiverOne.java
@@ -0,0 +1,27 @@
+package io.codingtest.codility;
+
+import java.util.*;
+
+public class FrogRiverOne {
+
+    public static void main(String[] args) {
+        int K = 3;
+        int[] input = new int[]{1, 3, 1, 3, 2, 1, 3};
+        int output = new FrogRiverOne().solution(K, input);
+        System.out.println(output);
+    }
+
+    public int solution(int X, int[] A) {
+        Set<Integer> checked = new HashSet<>();
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] <= X) {
+                checked.add(A[i]);
+            }
+            if (checked.size() == X) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/io/codingtest/codility/PermCheck.java
@@ -0,0 +1,25 @@
+package io.codingtest.codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermCheck {
+
+    public static void main(String[] args) {
+        int[] input = new int[]{4, 1, 3, 2};
+        int output = new PermCheck().solution(input);
+        System.out.println(output);
+    }
+
+    public int solution(int[] A) {
+        int sumOfIndex = 0;
+        int sumOfA = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            sumOfIndex += (i+1);
+            sumOfA += A[i];
+            set.add(A[i]);
+        }
+        return (sumOfIndex == sumOfA && A.length == set.size()) ? 1 : 0;
+    }
+}
